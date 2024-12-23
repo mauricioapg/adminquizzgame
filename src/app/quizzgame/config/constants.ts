4ff7d4809102139b27c9bd6f2ab3c6e86d606c2d
mauriciokoruja2 <mauricio@korujatec.com>
2024-12-23 13:47:43 -0300

    alterei URL da api

--- src/app/quizzgame/config/constants.ts
+++ src/app/quizzgame/config/constants.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://quizzgameapi-ji6nrrlm7q-uc.a.run.app";
+export const BASE_URL = "https://quizzgameapi-405502055586.us-central1.run.app";
