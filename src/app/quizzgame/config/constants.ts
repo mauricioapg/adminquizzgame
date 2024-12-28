a51c805bc39b2a310bbd944c0eedca31378e49a4
mauriciokoruja2 <mauricio@korujatec.com>
2024-12-28 17:50:51 -0300

    alterei URL

--- src/app/quizzgame/config/constants.ts
+++ src/app/quizzgame/config/constants.ts
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:8080";
-export const BASE_URL = "https://quizzgameapi-405502055586.us-central1.run.app";
+export const BASE_URL = "https://quizzgame-405502055586.us-central1.run.app";
